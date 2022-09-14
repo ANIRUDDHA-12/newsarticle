3945ec2529ca8350f6c1f9ec931853b4db5ac14b
saifmohammed888 <saifmohammed888@gmail.com>
2022-09-14 23:13:41 +0530

    Fixed Header issues

---
+++ home.js
@@ -0,0 +1,10 @@
+let sidebar = document.getElementById("sidebar");
+
+function showSidebar(){
+   sidebar.style.visibility = "visible"
+}
+
+function closeSidebar(){
+    console.log("m")
+    sidebar.style.visibility = "hidden"
+}
